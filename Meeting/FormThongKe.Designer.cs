2aff4ee946be5f7097ca6183156087350d64b9c5
vanlinh28032002@gmail.com <vanlinh28032002@gmail.com>
2024-05-22 23:46:35 +0700

    Thêm Form Thống Kê

---
+++ Meeting/FormThongKe.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Meeting
+{
+    partial class FormThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LbUserID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboUserID = new System.Windows.Forms.ComboBox();
+            this.txtCuocHop = new System.Windows.Forms.TextBox();
+            this.dgvUser = new System.Windows.Forms.DataGridView();
+            this.MeetingID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TimeStart = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UserID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtCuocHop);
+            this.splitContainer1.Panel1.Controls.Add(this.cboUserID);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.LbUserID);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvUser);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 114;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thống Kê";
+            // 
+            // LbUserID
+            // 
+            this.LbUserID.AutoSize = true;
+            this.LbUserID.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbUserID.Location = new System.Drawing.Point(12, 70);
+            this.LbUserID.Name = "LbUserID";
+            this.LbUserID.Size = new System.Drawing.Size(139, 22);
+            this.LbUserID.TabIndex = 1;
+            this.LbUserID.Text = "Mã Người Dùng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(481, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số lượng cuộc họp";
+            // 
+            // cboUserID
+            // 
+            this.cboUserID.FormattingEnabled = true;
+            this.cboUserID.Location = new System.Drawing.Point(157, 68);
+            this.cboUserID.Name = "cboUserID";
+            this.cboUserID.Size = new System.Drawing.Size(224, 24);
+            this.cboUserID.TabIndex = 3;
+            this.cboUserID.SelectedIndexChanged += new System.EventHandler(this.cboUserID_SelectedIndexChanged);
+            // 
+            // txtCuocHop
+            // 
+            this.txtCuocHop.Location = new System.Drawing.Point(668, 71);
+            this.txtCuocHop.Name = "txtCuocHop";
+            this.txtCuocHop.Size = new System.Drawing.Size(100, 22);
+            this.txtCuocHop.TabIndex = 4;
+            // 
+            // dgvUser
+            // 
+            this.dgvUser.AllowUserToAddRows = false;
+            this.dgvUser.AllowUserToDeleteRows = false;
+            this.dgvUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUser.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MeetingID,
+            this.Title,
+            this.Date,
+            this.TimeStart,
+            this.Link,
+            this.UserID});
+            this.dgvUser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvUser.Location = new System.Drawing.Point(0, 0);
+            this.dgvUser.Name = "dgvUser";
+            this.dgvUser.ReadOnly = true;
+            this.dgvUser.RowHeadersWidth = 51;
+            this.dgvUser.RowTemplate.Height = 24;
+            this.dgvUser.Size = new System.Drawing.Size(800, 332);
+            this.dgvUser.TabIndex = 0;
+            // 
+            // MeetingID
+            // 
+            this.MeetingID.DataPropertyName = "MeetingID";
+            this.MeetingID.HeaderText = "Mã Phòng";
+            this.MeetingID.MinimumWidth = 6;
+            this.MeetingID.Name = "MeetingID";
+            this.MeetingID.ReadOnly = true;
+            this.MeetingID.Width = 125;
+            // 
+            // Title
+            // 
+            this.Title.DataPropertyName = "Title";
+            this.Title.HeaderText = "Tên Phòng";
+            this.Title.MinimumWidth = 6;
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            this.Title.Width = 125;
+            // 
+            // Date
+            // 
+            this.Date.DataPropertyName = "Date";
+            this.Date.HeaderText = "Ngày Họp";
+            this.Date.MinimumWidth = 6;
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            this.Date.Width = 125;
+            // 
+            // TimeStart
+            // 
+            this.TimeStart.DataPropertyName = "TimeStart";
+            this.TimeStart.HeaderText = "Thời gian họp";
+            this.TimeStart.MinimumWidth = 6;
+            this.TimeStart.Name = "TimeStart";
+            this.TimeStart.ReadOnly = true;
+            this.TimeStart.Width = 125;
+            // 
+            // Link
+            // 
+            this.Link.DataPropertyName = "Link";
+            this.Link.HeaderText = "Link Phòng";
+            this.Link.MinimumWidth = 6;
+            this.Link.Name = "Link";
+            this.Link.ReadOnly = true;
+            this.Link.Width = 125;
+            // 
+            // UserID
+            // 
+            this.UserID.DataPropertyName = "UserID";
+            this.UserID.HeaderText = "Mã Người Dùng";
+            this.UserID.MinimumWidth = 6;
+            this.UserID.Name = "UserID";
+            this.UserID.ReadOnly = true;
+            this.UserID.Width = 125;
+            // 
+            // FormThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FormThongKe";
+            this.Text = "FormThongKe";
+            this.Load += new System.EventHandler(this.FormThongKe_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtCuocHop;
+        private System.Windows.Forms.ComboBox cboUserID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LbUserID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvUser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MeetingID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimeStart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Link;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserID;
+    }
+}
---
+++ Meeting/FormThongKe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meeting
+{
+    public partial class FormThongKe : Form
+    {
+        public FormThongKe()
+        {
+            InitializeComponent();
+        }
+
+        private void FormThongKe_Load(object sender, EventArgs e)
+        {
+            cboUserID.DisplayMember = "TaiKhoan";
+            cboUserID.ValueMember = "UserID";
+            cboUserID.DataSource = DataProvider.TruyVanLayDuLieu("SELECT * FROM UserLogin");
+
+            // ép chọn người đầu tiên
+            cboUserID.SelectedIndex = 0;
+            laySocuochoptheoUser();
+        }
+
+        private void laySocuochoptheoUser()
+        {
+            var sql = $"SELECT MeetingID, Title, Date, TimeStart, Link, UserID FROM Meeting WHERE UserID = '{cboUserID.SelectedValue.ToString()}'";
+
+            var dtUser = DataProvider.TruyVanLayDuLieu(sql);
+            dgvUser.DataSource = dtUser;
+            txtCuocHop.Text = dtUser.Rows.Count.ToString();
+        }
+
+        private void cboUserID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            laySocuochoptheoUser();
+        }
+    }
+}
